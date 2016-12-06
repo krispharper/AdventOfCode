c54289b34e4cf086c67ed6a4859b2ab2842342fa
Kris Harper <kris.p.harper@gmail.com>
2016-12-06 09:15:16 -0500

    Finished day 6

---
+++ 6/2.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+with open('input.txt') as f:
+    raw_data = [line.strip() for line in f]
+
+columns = []
+
+for i in range(len(raw_data[0])):
+    columns.append([])
+
+for d in raw_data:
+    for i in range(len(raw_data[0])):
+        columns[i].append(d[i])
+
+for c in columns:
+    print(Counter(c).most_common()[-1:][0][0], end='')
