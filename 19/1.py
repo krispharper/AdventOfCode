8d75681043998cc73433df1b57e1aa691bd21cb8
Kris Harper <kris.p.harper@gmail.com>
2016-12-21 03:47:16 +0000

    Finished day 19

--- 19/1.py
+++ 19/1.py
@@ -1,45 +1,13 @@
 from math import log
 
 
-def populate_dict(count):
-    for i in range(1, count + 1):
-        d[i] = 1
+def calculate1(n):
+    p = int(log(n, 2))
+    r = n - 2 ** p
+    return 2 * r + 1
 
 
-def loop(count):
-    populate_dict(count)
-    index = 1
-
-    while True:
-        new_index = find_next_index(index, count)
-
-        if new_index == index:
-            break
-
-        d[index] += d[new_index]
-        d[new_index] = 0
-        index = find_next_index(index, count)
-
-    return index
-
-
-def increment(index, count):
-    if index == len(d):
-        return 1
-    else:
-        return index + 1
-
-
-def find_next_index(index, count):
-    next_index = increment(index, count)
-
-    while not d[next_index]:
-        next_index = increment(next_index, count)
-
-    return next_index
-
-
-def calculate(n):
+def calculate2(n):
     p = int(log(n, 3))
     low = 3 ** p
     high = 3 ** (p + 1)
@@ -54,5 +22,5 @@ test_data = 5
 data = 3017957
 d = {}
 
-print(loop(data))
-print(calculate(data))
+print(calculate1(data))
+print(calculate2(data))
